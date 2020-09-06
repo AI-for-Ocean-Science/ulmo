a704bc98ff6c6054c6ebb8e24a75cd5038d424e3
profxj <xavier@ucolick.org>
2020-09-06 05:42:19 -0700

    memory leak?

--- ulmo/scripts/extract_modis.py
+++ ulmo/scripts/extract_modis.py
@@ -92,6 +92,8 @@ def extract_file(ifile, load_path, field_size=(128,128), nadir_offset=480,
     #if debug:
     #    print("f: {}, n_field = {}".format(ifile, len(fields)))
 
+    del sst, masks
+
     return np.stack(fields), np.stack(field_masks), np.stack(metadata)
 
 def main(pargs):
@@ -139,9 +141,9 @@ def main(pargs):
     with ProcessPoolExecutor(max_workers=n_cores) as executor:
         files = [f for f in os.listdir(load_path) if f.endswith('.nc')]
         if pargs.wolverine:
-            files = [f for f in os.listdir(load_path) if f.endswith('.nc')]*4
+            files = [f for f in os.listdir(load_path) if f.endswith('.nc')]*500
         elif pargs.debug:
-            files = files[0:5000]
+            files = files[0:1000]
         chunksize = len(files) // n_cores if len(files) // n_cores > 0 else 1
         answers = list(tqdm(executor.map(map_fn, files, chunksize=chunksize), total=len(files)))
 
