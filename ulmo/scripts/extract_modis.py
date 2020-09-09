077e75eec3622149d85737cb2f1ddde78772cde7
profxj <xavier@ucolick.org>
2020-09-09 12:32:01 -0700

    nfiles

--- ulmo/scripts/extract_modis.py
+++ ulmo/scripts/extract_modis.py
@@ -44,6 +44,7 @@ def parser(options=None):
     parser.add_argument('--nsub_files', type=int, default=1000, help='Number of files to process at a time')
     parser.add_argument("--debug", default=False, action="store_true", help="Debug?")
     parser.add_argument("--wolverine", default=False, action="store_true", help="Run on Wolverine")
+    parser.add_argument("--no_inpaint", default=False, action="store_true", help="Turn off inpainting?")
     args = parser.parse_args()
 
     if options is None:
@@ -55,7 +56,7 @@ def parser(options=None):
 
 def extract_file(ifile, load_path, field_size=(128,128), nadir_offset=480,
                  CC_max=0.05, qual_thresh=2, temp_bounds = (-2, 33), nrepeat=1,
-                 debug=False):
+                 inpaint=True, debug=False):
 
     filename = os.path.join(load_path, ifile)
 
@@ -71,7 +72,7 @@ def extract_file(ifile, load_path, field_size=(128,128), nadir_offset=480,
     lb = nadir_pix - nadir_offset
     ub = nadir_pix + nadir_offset
     sst = sst[:, lb:ub]
-    masks = masks[:, lb:ub]
+    masks = masks[:, lb:ub].astype(np.uint8)
 
     # Random clear rows, cols
     rows, cols, clear_fracs = extract.random_clear(masks, field_size[0], CC_max=CC_max,
@@ -83,9 +84,16 @@ def extract_file(ifile, load_path, field_size=(128,128), nadir_offset=480,
     fields, field_masks = [], []
     metadata = []
     for r, c, clear_frac in zip(rows, cols, clear_fracs):
-        # SST and mask
-        fields.append(sst[r:r+field_size[0], c:c+field_size[1]].astype(np.float32))
-        field_masks.append(masks[r:r+field_size[0], c:c+field_size[1]])
+        # Inpaint?
+        field = sst[r:r+field_size[0], c:c+field_size[1]]
+        mask = masks[r:r+field_size[0], c:c+field_size[1]]
+        if inpaint:
+            field, _ = pp_utils.preproc_field(field, mask, only_inpaint=True)
+        if field is None:
+            continue
+        # Append SST and mask
+        fields.append(field.astype(np.float32))
+        field_masks.append(mask)
         # meta
         row, col = r, c + lb
         lat = latitude[row + field_size[0] // 2, col + field_size[1] // 2]
@@ -101,14 +109,17 @@ def extract_file(ifile, load_path, field_size=(128,128), nadir_offset=480,
 def main(pargs):
     """ Run
     """
+    # Filenames
+    istr = 'F' if pargs.no_inpaint else 'T'
     load_path = f'/Volumes/Aqua-1/MODIS/night/night/{pargs.year}'
     save_path = (f'/Volumes/Aqua-1/MODIS/uri-ai-sst/xavier/MODIS_{pargs.year}'
-                 f'_{pargs.clear_threshold}clear_{pargs.field_size}x{pargs.field_size}.h5')
+                 f'_{pargs.clear_threshold}clear_{pargs.field_size}x{pargs.field_size}_inpaint{istr}.h5')
     if pargs.wolverine:
         load_path = f'/home/xavier/Projects/Oceanography/AI/OOD'
         save_path = (f'TST_{pargs.year}'
             f'_{pargs.clear_threshold}clear_{pargs.field_size}x{pargs.field_size}.h5')
 
+    # Setup for preproc
     map_fn = partial(extract_file,
                      load_path=load_path,
                      field_size=(pargs.field_size, pargs.field_size),
@@ -117,8 +128,8 @@ def main(pargs):
                      nadir_offset=pargs.nadir_offset,
                      temp_bounds=(pargs.temp_lower_bound, pargs.temp_upper_bound),
                      nrepeat=pargs.nrepeat,
+                     inpaint=not pargs.no_inpaint,
                      debug=pargs.debug)
-
 
     '''
     if pargs.debug:
@@ -148,9 +159,10 @@ def main(pargs):
     # Limit number of files to 10000
     files = [f for f in os.listdir(load_path) if f.endswith('.nc')]
     if pargs.wolverine:
-        files = [f for f in os.listdir(load_path) if f.endswith('.nc')] * 500
+        files = [f for f in os.listdir(load_path) if f.endswith('.nc')] * 50
     elif pargs.debug:
-        files = files[6000:8000]
+        #files = files[6000:8000]
+        files = files[0:100]
 
     nloop = len(files) // pargs.nsub_files + ((len(files) % pargs.nsub_files) > 0)
     print('nloop: {}'.format(nloop))
@@ -164,8 +176,8 @@ def main(pargs):
 
         with ProcessPoolExecutor(max_workers=n_cores) as executor:
             chunksize = len(sub_files) // n_cores if len(sub_files) // n_cores > 0 else 1
-            if pargs.debug:
-                chunksize = 100
+            #if pargs.debug:
+            #    chunksize = 100
             answers = list(tqdm(executor.map(map_fn, sub_files,
                                              chunksize=chunksize), total=len(sub_files)))
 
@@ -187,6 +199,6 @@ def main(pargs):
     with h5py.File(save_path, 'w') as f:
         #f.create_dataset('fields', data=fields.astype(np.float32))
         f.create_dataset('fields', data=fields)
-        f.create_dataset('masks', data=masks.astype(np.int8))
+        f.create_dataset('masks', data=masks.astype(np.uint8))
         dset = f.create_dataset('metadata', data=metadata.astype('S'))
         dset.attrs['columns'] = columns
