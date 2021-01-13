179283413f07a1f8a071ae23dadaeee186595310
profxj <xavier@ucolick.org>
2021-01-13 10:52:46 -0800

    still at it

--- ulmo/llc/slurp.py
+++ ulmo/llc/slurp.py
@@ -40,19 +40,23 @@ def write_xr(xr_d, outfile, strip_coord=True, encode=True):
         encoding['Theta'] = {'dtype': 'int16', 'scale_factor': 1e-3,
                              'add_offset': 10., 'zlib': True, 
                              '_FillValue': -32767,
-                             'missing_value': -32767}
+                             'missing_value': -32768}
         encoding['U'] = {'dtype': 'int16', 'scale_factor': 1e-3,
                              'add_offset': 0., 'zlib': True, 
                              '_FillValue': -32767,
-                             'missing_value': -32767}
+                             'missing_value': -32768}
         encoding['V'] = {'dtype': 'int16', 'scale_factor': 1e-3,
                              'add_offset': 0., 'zlib': True, 
                              '_FillValue': -32767,
-                             'missing_value': -32767}
+                             'missing_value': -32768}
+        encoding['W'] = {'dtype': 'int16', 'scale_factor': 1e-3,
+                             'add_offset': 0., 'zlib': True, 
+                             '_FillValue': -32767,
+                             'missing_value': -32768}
         encoding['Salt'] = {'dtype': 'int16', 'scale_factor': 1e-3,
                              'add_offset': 30., 'zlib': True, 
                              '_FillValue': -32767,
-                             'missing_value': -32767}
+                             'missing_value': -32768}
     else:
         encoding = None
 
