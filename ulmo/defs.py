15219262d7d2bf2b40b1dd878a49ecb70f842246
profxj <xavier@ucolick.org>
2021-03-16 13:29:30 -0700

    fussing with latents

--- ulmo/defs.py
+++ ulmo/defs.py
@@ -2,6 +2,7 @@
 
 import os
 
+# MODIS L2
 extract_path = os.path.join(os.getenv("SST_OOD"), 'Extractions')
 model_path = os.path.join(os.getenv("SST_OOD"), 'Models')
-eval_path = os.path.join(os.getenv("SST_OOD"), 'Evaluations')
+eval_path = os.path.join(os.getenv("SST_OOD"), 'MODIS_L2', 'Evaluations')
