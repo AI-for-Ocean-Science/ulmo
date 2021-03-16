c58b291083faeda0e020cb4b8d61bf5af83c103d
profxj <xavier@ucolick.org>
2021-03-16 13:39:06 -0700

    mk those dir

--- runs/LLC/llc_evaluate_test.py
+++ runs/LLC/llc_evaluate_test.py
@@ -10,11 +10,17 @@ print("Model loaded!")
 
 # Download preproc file for speed
 preproc_folder = 'PreProc'
+if not os.path.isdir(preproc_folder):
+    os.mkdir(preproc_folder)
 data_file = os.path.join(preproc_folder, 'LLC_uniform_preproc_test.h5') 
 ulmo_io.s3.Bucket('llc').download_file(data_file, data_file)
 
 # Output file
-log_prob_file = 'Evaluations/LLC_uniform_test_std_log_prob.h5'
+output_folder = 'Evaluations'
+if not os.path.isdir(output_folder):
+    os.mkdir(output_folder)
+log_prob_file = os.path.join(output_folder, 
+                             'LLC_uniform_test_std_log_prob.h5')
 
 # Run
 pae.compute_log_probs(data_file, 'valid', 
