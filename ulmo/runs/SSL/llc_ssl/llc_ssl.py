4d791c74770f0083e045c52df535fc4017858581
karlschwarz <eguo1@ucsc.edu>
2021-06-17 03:42:12 +0000

    comet is used to visualize the training process.

--- ulmo/runs/SSL/llc_ssl/llc_ssl.py
+++ ulmo/runs/SSL/llc_ssl/llc_ssl.py
@@ -8,13 +8,15 @@ import pandas as pd
 from tqdm.auto import trange
 import argparse
 
+from comet_ml import Experiment
+
 import torch
 
 from ulmo.ssl.util import adjust_learning_rate
 from ulmo.ssl.util import set_optimizer, save_model
 
 from ulmo.ssl.train_util import Params, option_preprocess
-from ulmo.ssl.train_util import modis_loader, set_model
+from ulmo.ssl.train_util import modis_loader_v2, set_model
 from ulmo.ssl.train_util import train_model
 from ulmo.ssl import train_modis
 
@@ -38,13 +40,21 @@ def main_train(opt_path: str):
     opt = option_preprocess(opt)
 
     # build data loader
-    train_loader = modis_loader(opt)
+    train_loader = modis_loader_v2(opt)
 
     # build model and criterion
     model, criterion = set_model(opt, cuda_use=opt.cuda_use)
 
     # build optimizer
     optimizer = set_optimizer(opt, model)
+    
+    # comet
+    experiment = Experiment(
+            api_key="CMd7nIgpIzRvoH90zGB3vA1Dk",
+            project_name="llc_modis_2012", 
+            workspace="edwkuo",
+    )
+    experiment.log_parameters(opt.dict)
     
     # training routine
     for epoch in trange(1, opt.epochs + 1):
@@ -56,7 +66,11 @@ def main_train(opt_path: str):
         loss = train_model(train_loader, model, criterion, optimizer, epoch, opt, cuda_use=opt.cuda_use)
         time2 = time.time()
         print('epoch {}, total time {:.2f}'.format(epoch, time2 - time1))
-
+        
+        # comet
+        experiment.log_metric('loss', loss, step=epoch)
+        experiment.log_metric('learning_rate', optimizer.param_groups[0]['lr'], step=epoch)
+        
         if epoch % opt.save_freq == 0:
             save_file = os.path.join(
                 opt.save_folder, 'ckpt_epoch_{epoch}.pth'.format(epoch=epoch))
