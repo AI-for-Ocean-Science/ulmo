3562e94eab8c0cfb47711f017e605f65f5f9d193
profxj <xavier@ucolick.org>
2021-06-19 13:55:00 -0700

    refactor and more

---
+++ ulmo/ssl/analysis.py
@@ -0,0 +1,72 @@
+""" Analysis methods for self-supervised learning 
+"""
+import numpy as np
+
+import pandas
+from matplotlib import pyplot as plt
+
+import umap
+
+from ulmo import io as ulmo_io
+from ulmo.plotting import plotting
+from ulmo.utils import catalog as cat_utils
+
+from IPython import embed
+
+def do_umap(latents:np.ndarray, train:np.ndarray, 
+         valid:np.ndarray, valid_tbl:pandas.DataFrame,
+         fig_root='', debug=False, write_to_file=str):
+
+    # UMAP me
+    print("Running UMAP..")
+    reducer_umap = umap.UMAP()
+    latents_mapping = reducer_umap.fit(latents[train])
+    print("Done")
+
+    # Apply to embedding
+    print("Applying to the valid images")
+    valid_embedding = latents_mapping.transform(latents[valid])
+    print("Done")
+
+    # Quick figures
+    if len(fig_root) > 0:
+        print("Generating plots")
+        num_samples = train.size
+        point_size = 20.0 / np.sqrt(num_samples)
+        dpi = 100
+        width, height = 800, 800
+
+        plt.figure(figsize=(width//dpi, height//dpi))
+        plt.scatter(latents_mapping.embedding_[:, 0], 
+            latents_mapping.embedding_[:, 1], s=point_size)
+        plt.savefig(fig_root+'_train_UMAP.png')
+
+        # New plot
+        num_samplesv = latents[valid].shape[0]
+        point_sizev = 1.0 / np.sqrt(num_samplesv)
+        plt.figure(figsize=(width//dpi, height//dpi))
+        ax = plt.gca()
+        img = ax.scatter(valid_embedding[:, 0], 
+                valid_embedding[:, 1], s=point_sizev,
+            c=valid_tbl.LL, cmap='jet', vmin=-1000)
+        cb = plt.colorbar(img, pad=0.)
+        cb.set_label('LL', fontsize=20.)
+        #
+        ax.set_xlabel(r'$U_0$')
+        ax.set_ylabel(r'$U_1$')
+        plotting.set_fontsize(ax, 15.)
+        #
+        plt.savefig(fig_root+'_valid_UMAP.png', dpi=300)
+
+    # Save to Table
+    print("Writing to the Table")
+    valid_tbl['U0'] = valid_embedding[:, 0]
+    valid_tbl['U1'] = valid_embedding[:, 1]
+
+    # Vet
+    assert cat_utils.vet_main_table(valid_tbl, cut_prefix='modis_')
+
+    # Final write
+    if write_to_file is not None:
+        ulmo_io.write_main_table(valid_tbl, write_to_file)
+    	
