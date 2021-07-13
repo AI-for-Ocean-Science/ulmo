343b8d8af799d5e5821f47d022f70c114670c54d
karlschwarz <eguo1@ucsc.edu>
2021-07-13 19:18:20 +0000

    modis main_train_valid function is implemented.

--- ulmo/ssl/train_modis.py
+++ ulmo/ssl/train_modis.py
@@ -45,6 +45,49 @@ def main_train(opt_path: str):
     save_file = os.path.join(
         opt.save_folder, 'last.pth')
     save_model(model, optimizer, opt, opt.epochs, save_file)
+    
+def main_train_valid(opt_path: str):
+    # loading parameters json file
+    opt = Params(opt_path)
+    opt = option_preprocess(opt)
+
+    # build data loader
+    train_loader = modis_loader(opt)
+    valid_loader = modis_loader(opt, valid=True)
+
+    # build model and criterion
+    model, criterion = set_model(opt)
+
+    # build optimizer
+    optimizer = set_optimizer(opt, model)
+
+    # training routine
+    for epoch in trange(1, opt.epochs + 1):
+
+        adjust_learning_rate(opt, optimizer, epoch)
+
+        # train for one epoch
+        time1 = time.time()
+        loss = train_model(train_loader, model, criterion, optimizer, epoch, opt)
+        time2 = time.time()
+        print('epoch {}, total time {:.2f}'.format(epoch, time2 - time1))
+        
+        if epoch % opt.valid_freq == 0:
+            epoch_valid = epoch // opt.valid_freq
+            time1_valid = time.time()
+            loss_valid = valid_model(valid_loader, model, criterion, epoch_valid, opt)
+            time2_valid = time.time()
+            print('valid epoch {}, total time {:.2f}'.format(epoch_valid, time2_valid - time1_valid))
+
+        if epoch % opt.save_freq == 0:
+            save_file = os.path.join(
+                opt.save_folder, 'ckpt_epoch_{epoch}.pth'.format(epoch=epoch))
+            save_model(model, optimizer, opt, epoch, save_file)
+
+    # save the last model
+    save_file = os.path.join(
+        opt.save_folder, 'last.pth')
+    save_model(model, optimizer, opt, opt.epochs, save_file)
 
 if __name__ == "__main__":
     
