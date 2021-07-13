6bf6adf62f44f8cfe2d0bc4ea18eeb012f18ecdb
katmar4141 <k.gallmeier@columbia.edu>
2021-07-13 18:11:41 +0000

    adding new scripts

--- ulmo/llc/add_noise.py
+++ ulmo/llc/add_noise.py
@@ -1,16 +1,22 @@
-# Add SST-dependent noise to 'LLC_modis2012_test_SST_noise_preproc.h5' file
+# Add SST-dependent noise to 'LLC_modis2012_SST_noise_demean_preproc.h5' file
 # The file is a copied verision of 'LLC_modis2012_test_preproc.h5' file
 
 import h5py
 import numpy as np
 
-with h5py.File( 'LLC_modis2012_test_SST_noise_preproc.h5', 'r+') as f:
-     mdata = f['valid_metadata'][()]
-     mean_T = mdata[:,7]
+with h5py.File( '/home/jovyan/ulmo/ulmo/notebooks/LLC_modis2012_SST_noise_demean_preproc.h5', 'r+') as f:
+    mdata = f['valid_metadata'][()]
+    mean_T = mdata[:,7]
      
-     # loop through all cutouts
-     for idx in range( mdata.shape[0] ):
-             tmp = float( mean_T[idx].decode() )
-             sig = 0.031 + 0.0048*tmp
-             noise = np.random.normal(0., sig, (64,64))
-             f['valid'][idx, 0, ...] = f['valid'][idx, 0, ...] + noise
+    # loop through all cutouts
+    for idx in range( mdata.shape[0] ):
+        tmp = float( mean_T[idx].decode() )
+        sig = 0.031 + 0.0048*tmp
+        noise = np.random.normal(0., sig, (64,64))
+        f['valid'][idx, 0, ...] = f['valid'][idx, 0, ...] + noise
+        
+        # demean
+        img = f['valid'][idx, 0, ...]
+        mean = np.mean( img )
+        f['valid'][idx, 0, ...] = img - mean*np.ones_like( img )
+    
