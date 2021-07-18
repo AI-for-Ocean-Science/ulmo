9e110fa632067e3b719f79ecc0d4c306dc669b6f
profxj <xavier@ucolick.org>
2021-07-18 05:33:18 -0700

    ready to rock

--- ulmo/runs/VIIRS/viirs_ulmo.py
+++ ulmo/runs/VIIRS/viirs_ulmo.py
@@ -2,20 +2,20 @@
 import os
 import glob
 import numpy as np
-import subprocess 
+import subprocess
 
 import pandas
 import h5py
-from skimage.restoration import inpaint 
+from skimage.restoration import inpaint
 
 from sklearn.utils import shuffle
 
 from ulmo import io as ulmo_io
-from ulmo.preproc import io as pp_io 
+from ulmo.preproc import io as pp_io
 from ulmo.preproc import utils as pp_utils
 from ulmo.viirs import extract as viirs_extract
 from ulmo.modis import utils as modis_utils
-from ulmo.analysis import evaluate as ulmo_evaluate 
+from ulmo.analysis import evaluate as ulmo_evaluate
 from ulmo.utils import catalog as cat_utils
 
 import argparse
@@ -31,19 +31,21 @@ from IPython import embed
 #tbl_file_2014 = 's3://viirs/Tables/VIIRS_2014_std.parquet'
 s3_bucket = 's3://viirs'
 
+
 def parse_option():
     """
     This is a function used to parse the arguments in the training.
-    
+
     Returns:
         args: (dict) dictionary of the arguments.
     """
     parser = argparse.ArgumentParser("VIIRS")
-    parser.add_argument("--task", type=str, 
+    parser.add_argument("--task", type=str,
                         help="task to execute: 'download','evaluate', 'umap'.")
     parser.add_argument("--year", type=int, help="Year to work on")
+    parser.add_argument("--n_cores", type=int, help="Number of CPU to use")
     args = parser.parse_args()
-    
+
     return args
 
 
@@ -69,8 +71,8 @@ def viirs_get_data_into_s3(year=2014, day1=1, debug=False):
             ulmo_io.upload_file_to_s3(nc_file, s3_file)
             # Remove
             os.remove(nc_file)
-    
-    #for ss in range(365):
+
+    # for ss in range(365):
     ndays = 366
     for ss in range(day1-1, ndays):
         iday = ss + 1
@@ -78,13 +80,13 @@ def viirs_get_data_into_s3(year=2014, day1=1, debug=False):
         sday = str(iday).zfill(3)
         # Popen
         pw = subprocess.Popen([
-            'wget', '--no-check-certificate', '--user=mskelm', 
-            '--password=B3oNdVKkgWW0NmPoLyS', 
-            '-r', '-nc', '-np',  '-nH', '-nd', '-A', 
+            'wget', '--no-check-certificate', '--user=mskelm',
+            '--password=B3oNdVKkgWW0NmPoLyS',
+            '-r', '-nc', '-np',  '-nH', '-nd', '-A',
             '{}*.nc'.format(str(year)),
-            #'*.nc', 
+            # '*.nc',
             'https://podaac-tools.jpl.nasa.gov/drive/files/allData/ghrsst/data/GDS2/L2P/VIIRS_NPP/OSPO/v2.61/{}/{}/'.format(
-                year,sday)])
+                year, sday)])
         if ss == 0:
             pass
         else:
@@ -102,7 +104,7 @@ def viirs_get_data_into_s3(year=2014, day1=1, debug=False):
         push_to_s3(nc_files, pvday, year)
 
 
-def viirs_extract(debug=False, year=2014, 
+def viirs_extract(debug=False, year=2014,
                   n_cores=20, nsub_files=5000,
                   ndebug_files=0):
     """Extract "cloud free" images for a given year
@@ -119,8 +121,8 @@ def viirs_extract(debug=False, year=2014,
 
     # Pre-processing (and extraction) settings
     pdict = pp_io.load_options('viirs_std')
-    
-    # 
+
+    #
     print("Grabbing the file list")
     all_viirs_files = ulmo_io.list_of_bucket_files('viirs')
     files = []
@@ -149,7 +151,7 @@ def viirs_extract(debug=False, year=2014,
     # Local file for writing
     f_h5 = h5py.File(save_path, 'w')
     print("Opened local file: {}".format(save_path))
-    
+
     nloop = len(files) // nsub_files + ((len(files) % nsub_files) > 0)
     metadata = None
     for kk in range(nloop):
@@ -162,7 +164,8 @@ def viirs_extract(debug=False, year=2014,
         sub_files = files[i0:i1]
 
         with ProcessPoolExecutor(max_workers=n_cores) as executor:
-            chunksize = len(sub_files) // n_cores if len(sub_files) // n_cores > 0 else 1
+            chunksize = len(
+                sub_files) // n_cores if len(sub_files) // n_cores > 0 else 1
             answers = list(tqdm(executor.map(map_fn, sub_files,
                                              chunksize=chunksize), total=len(sub_files)))
 
@@ -173,12 +176,13 @@ def viirs_extract(debug=False, year=2014,
         if metadata is None:
             metadata = np.concatenate([item[2] for item in answers])
         else:
-            metadata = np.concatenate([metadata]+[item[2] for item in answers], axis=0)
+            metadata = np.concatenate([metadata]+[item[2]
+                                                  for item in answers], axis=0)
         del answers
 
         # Write
         if kk == 0:
-            f_h5.create_dataset('fields', data=fields, 
+            f_h5.create_dataset('fields', data=fields,
                                 compression="gzip", chunks=True,
                                 maxshape=(None, fields.shape[1], fields.shape[2]))
             f_h5.create_dataset('inpainted_masks', data=inpainted_masks,
@@ -187,19 +191,19 @@ def viirs_extract(debug=False, year=2014,
         else:
             # Resize
             for key in ['fields', 'inpainted_masks']:
-                f_h5[key].resize((f_h5[key].shape[0] + fields.shape[0]), axis=0)
+                f_h5[key].resize(
+                    (f_h5[key].shape[0] + fields.shape[0]), axis=0)
             # Fill
             f_h5['fields'][-fields.shape[0]:] = fields
             f_h5['inpainted_masks'][-fields.shape[0]:] = inpainted_masks
-    
 
     # Metadata
-    columns = ['filename', 'row', 'column', 'latitude', 'longitude', 
+    columns = ['filename', 'row', 'column', 'latitude', 'longitude',
                'clear_fraction']
     dset = f_h5.create_dataset('metadata', data=metadata.astype('S'))
     dset.attrs['columns'] = columns
     # Close
-    f_h5.close() 
+    f_h5.close()
 
     # Table time
     viirs_table = pandas.DataFrame()
@@ -210,8 +214,10 @@ def viirs_extract(debug=False, year=2014,
     viirs_table['lon'] = [float(item[4]) for item in metadata]
     viirs_table['clear_fraction'] = [float(item[5]) for item in metadata]
     viirs_table['field_size'] = pdict['field_size']
-    basefiles = [os.path.basename(ifile) for ifile in viirs_table.filename.values]
-    viirs_table['datetime'] = modis_utils.times_from_filenames(basefiles, ioff=-1, toff=0)
+    basefiles = [os.path.basename(ifile)
+                 for ifile in viirs_table.filename.values]
+    viirs_table['datetime'] = modis_utils.times_from_filenames(
+        basefiles, ioff=-1, toff=0)
     viirs_table['ex_filename'] = s3_filename
 
     # Vet
@@ -219,29 +225,27 @@ def viirs_extract(debug=False, year=2014,
 
     # Final write
     ulmo_io.write_main_table(viirs_table, tbl_file)
-    
+
     # Push to s3
     print("Pushing to s3")
     ulmo_io.upload_file_to_s3(save_path, s3_filename)
-    #print("Run this:  s3 put {} s3://modis-l2/Extractions/{}".format(
+    # print("Run this:  s3 put {} s3://modis-l2/Extractions/{}".format(
     #    save_path, save_path))
-    #process = subprocess.run(['s4cmd', '--force', '--endpoint-url',
-    #    'https://s3.nautilus.optiputer.net', 'put', save_path, 
+    # process = subprocess.run(['s4cmd', '--force', '--endpoint-url',
+    #    'https://s3.nautilus.optiputer.net', 'put', save_path,
     #    s3_filename])
 
 
-def viirs_2014_preproc(debug=False, n_cores=20):
+def viirs_preproc(year=2014, debug=False, n_cores=20):
     """Pre-process the files
 
     Args:
         n_cores (int, optional): Number of cores to use
     """
-    if debug:
-        tbl_file = 's3://viirs/Tables/VIIRS_2014_tst.parquet'
-    else:
-        tbl_file = tbl_file_2014
+    # Table
+    tbl_file = f's3://viirs/Tables/VIIRS_{year}_std.parquet'
     viirs_tbl = ulmo_io.load_main_table(tbl_file)
-    viirs_tbl = pp_utils.preproc_tbl(viirs_tbl, 1., 
+    viirs_tbl = pp_utils.preproc_tbl(viirs_tbl, 1.,
                                      's3://viirs',
                                      preproc_root='viirs_std',
                                      inpainted_mask=True,
@@ -254,79 +258,50 @@ def viirs_2014_preproc(debug=False, n_cores=20):
     # Final write
     ulmo_io.write_main_table(viirs_tbl, tbl_file)
 
-def viirs_2014_evaluate(debug=False, model='modis-l2-std'):
-    """Evaluate the VIIRS 2014 data using Ulmo
+
+def viirs_evaluate(year=2014, debug=False, model='modis-l2-std'):
+    """Evaluate the VIIRS data using Ulmo
 
     Args:
         debug (bool, optional): [description]. Defaults to False.
         model (str, optional): [description]. Defaults to 'modis-l2-std'.
     """
-
-    if debug:
-        tbl_file = 's3://viirs/Tables/VIIRS_2014_tst.parquet'
-    else:
-        tbl_file = tbl_file_2014
-
-    # Load Ulmo
+    # Load table
+    tbl_file = f's3://viirs/Tables/VIIRS_{year}_std.parquet'
     viirs_tbl = ulmo_io.load_main_table(tbl_file)
 
     # Evaluate
     print("Starting evaluating..")
-    viirs_tbl = ulmo_evaluate.eval_from_main(viirs_tbl, model=model)
+    viirs_tbl = ulmo_evaluate.eval_from_main(viirs_tbl, 
+                                             model=model)
 
-    # Write 
+    # Write
     assert cat_utils.vet_main_table(viirs_tbl)
     ulmo_io.write_main_table(viirs_tbl, tbl_file)
     print("Done evaluating..")
 
 
-def main(flg):
-    if flg== 'all':
-        flg= np.sum(np.array([2 ** ii for ii in range(25)]))
-    else:
-        flg= int(flg)
-
-    # VIIRS download
-    if flg & (2**0):
-        viirs_get_data_into_s3(debug=False)
-
-    # VIIRS extract test
-    if flg & (2**1):
-        viirs_extract_2014(debug=True, n_cores=10, nsub_files=2000, ndebug_files=5000)
-
-    # VIIRS extract
-    if flg & (2**2):
-        viirs_extract_2014(n_cores=20, nsub_files=5000)
-
-    # VIIRS preproc test
-    if flg & (2**3):
-        viirs_2014_preproc(debug=True, n_cores=10)
-
-    # VIIRS preproc for reals
-    if flg & (2**4):
-        viirs_2014_preproc(n_cores=20)
-
-    # VIIRS eval test
-    if flg & (2**5): # 32
-        viirs_2014_evaluate(debug=True)
-
-    # VIIRS eval 
-    if flg & (2**6): # 64
-        viirs_2014_evaluate()
-
-
-    # MODIS pre-proc
-    #if flg & (2**2):
-    #    modis_day_evaluate()
-
-
 if __name__ == "__main__":
     # get the argument of training.
     pargs = parse_option()
-    
+
     if pargs.task == 'download':
         print("Downloading Starts.")
         viirs_get_data_into_s3(year=pargs.year)
         print("Downloading Ends.")
+    elif pargs.task == 'extract':
+        print("Extraction Starts.")
+        viirs_extract(year=pargs.year,
+                      n_cores=pargs.n_cores)
+        print("Extraction Ends.")
+    elif pargs.task == 'preproc':
+        print("PreProc Starts.")
+        viirs_preproc(year=pargs.year,
+                      n_cores=pargs.n_cores)
+        print("PreProc Ends.")
+    elif pargs.task == 'eval':
+        print("Evaluation Starts.")
+        viirs_evaluate(year=pargs.year)
+        print("Evaluation Ends.")
     else:
         raise IOError("Bad choice")
