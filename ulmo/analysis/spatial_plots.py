182037f273b2ea94dea7c3c3b256bb153f41a242
katmar4141 <k.gallmeier@columbia.edu>
2021-08-05 18:20:46 +0000

    Upload new function

--- ulmo/analysis/spatial_plots.py
+++ ulmo/analysis/spatial_plots.py
@@ -804,3 +804,158 @@ def show_spatial_diff(sub_tbl1:pandas.DataFrame, sub_tbl2:pandas.DataFrame,
         plt.show()
 
     return ax
+
+
+
+def evals_to_healpix1(eval_tbl, nside, mask=True):
+    """
+    Generate a healpix map of where the input
+    MHW Systems are located on the globe
+    Parameters
+    ----------
+    mhw_sys : pandas.DataFrame
+    nside : int  # nside is a number that sets the resolution of map
+    mask : bool, optional
+    Returns
+    -------
+    healpix_array : hp.ma (number of cutouts)
+    lats : np.ndarray
+    lons : np.ndarray
+    """
+    # Grab lats, lons
+    lats = eval_tbl.lat.values
+    lons = eval_tbl.lon.values
+
+    # Healpix coords
+    theta = (90 - lats) * np.pi / 180. 
+    phi = lons * np.pi / 180.
+    idx_all = hp.pixelfunc.ang2pix(nside, theta, phi) 
+
+    # Count events
+    npix_hp = hp.nside2npix(nside)
+    all_events = np.ma.masked_array(np.zeros(npix_hp, dtype='int'))  
+
+    for i, idx in enumerate(idx_all):
+        all_events[idx] += 1
+
+    zero = all_events == 0 
+    
+    float_events = all_events.astype(float)
+
+
+    # Mask
+    hpma = hp.ma(float_events)
+    if mask:  # if you want to mask float_events
+        hpma.mask = zero # current mask set to zero array, where Trues (no events) are masked
+
+    # Angles
+    hp_lons, hp_lats = hp.pixelfunc.pix2ang(nside, np.arange(npix_hp), lonlat=True)
+
+    # Return
+    return hpma, hp_lons, hp_lats
+
+
+
+def show_spatial_diff1(sub_tbl1:pandas.DataFrame, sub_tbl2:pandas.DataFrame,
+                 nside=64, use_log=True, 
+                 use_mask=True,
+                 lbl1=None, lbl2=None, figsize=(24,16), 
+                 color ='coolwarm', show=True):
+    """Generate a global map of the location of the input
+    cutouts
+
+    Args:
+        main_tbl (pandas.DataFrame): table of cutouts
+        nside (int, optional): [description]. Defaults to 64.
+        use_log (bool, optional): [description]. Defaults to True.
+        use_mask (bool, optional): [description]. Defaults to True.
+        lbl ([type], optional): [description]. Defaults to None.
+        figsize (tuple, optional): [description]. Defaults to (12,8).
+        color (str, optional): [description]. Defaults to 'Reds'.
+        show (bool, optional): If True, show on the screen.  Defaults to True
+
+    Returns:
+        matplotlib.Axis: axis holding the plot
+    """
+    # Healpix me
+    hp_events1, hp_lons1, hp_lats1= evals_to_healpix1(
+        sub_tbl1, nside, mask=use_mask)
+    
+    hp_events2, hp_lons2, hp_lats2= evals_to_healpix1(
+        sub_tbl2, nside, mask=use_mask)
+    
+    # Figure
+    
+    fig = plt.figure(figsize=figsize)
+    plt.clf()
+
+    tformM = ccrs.Mollweide()
+    tformP = ccrs.PlateCarree()
+
+    ax = plt.axes(projection=tformM)
+
+    cm = plt.get_cmap(color)
+    
+    # Masks
+    
+    
+    a = (hp_events1.mask == False)
+    b = (hp_events2.mask == False)
+    both = a & (a == b)
+    
+    
+    good1 = a & ( ~both)
+    good2 = b & ( ~both)
+    
+    
+    # Cut
+    img1 = plt.scatter(x=hp_lons1[good1],
+        y=hp_lats1[good1],
+        c= cm(0),
+        s=5,
+        transform=tformP)
+    
+    img2 = plt.scatter(x=hp_lons2[good2],
+        y=hp_lats2[good2],
+        c=cm(1),
+        s=5,
+        transform=tformP)
+    
+    img3 = plt.scatter(x=hp_lons2[both],
+        y=hp_lats2[both],
+        c=hp_events1[both] - hp_events2[both], vmin = -500, vmax = 500,
+        cmap=cm,
+        s=5,
+        transform=tformP) 
+    #c=hp_events2[both]
+
+    # Colorbar
+    cb = plt.colorbar(img3, orientation='vertical', location = 'right',shrink = 0.25)
+    
+    if lbl1 is not None and lbl2 is not None:
+        clbl='{} - {}'.format(lbl1, lbl2)
+        cb.set_label(clbl, fontsize=20.)
+    cb.ax.tick_params(labelsize=17)
+    
+
+    # Coast lines
+    ax.coastlines(zorder=10)
+    ax.set_global()
+
+    gl = ax.gridlines(crs=ccrs.PlateCarree(), linewidth=1, 
+        color='black', alpha=0.5, linestyle=':', draw_labels=True)
+    gl.xlabels_top = False
+    gl.ylabels_left = True
+    gl.ylabels_right=False
+    gl.xlines = True
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+    gl.xlabel_style = {'color': 'black'}# 'weight': 'bold'}
+    gl.ylabel_style = {'color': 'black'}# 'weight': 'bold'}
+
+
+    # Layout and save
+    if show:
+        plt.show()
+
+    return ax
