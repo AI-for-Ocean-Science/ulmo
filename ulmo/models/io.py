78917a9978c5abac25e5f597199124b4005490ee
karlschwarz <eguo1@ucsc.edu>
2021-08-12 03:57:57 +0000

    llc_modis_l2_extension function used to load general models is added.

--- ulmo/models/io.py
+++ ulmo/models/io.py
@@ -63,3 +63,62 @@ def load_modis_l2(flavor='std', datadir=None, local=False):
 
     # Return
     return pae
+
+def load_modis_l2_extension(flavor='std', datadir=None, filepath=None, local=False):
+    """Extension of the Load up the MODIS L2 model.
+
+    Args:
+        flavor (str, optional): Type of model trained. Defaults to 'std'.
+        datadir (str, optional): Path to data file for the model. Defaults to None.
+        filepath (str, optional): Path to the (train, valid) data set for the model.
+        local (bool, optional): If True, use local storage. Defaults to False.
+
+    Returns:
+        ulmo.ood.ProbablisticAutoencoder: the PAE
+    """
+    # Avoid circular imports
+
+    # TODO -- Allow for s3
+
+    #
+    if flavor == 'loggrad':
+        if datadir is None:
+            datadir = './Models/R2019_2010_128x128_loggrad'
+        if filepath is None:
+            filepath = 'PreProc/MODIS_R2019_2010_95clear_128x128_preproc_loggrad.h5'
+    elif flavor == 'std':
+        if datadir is None:
+            if local:
+                datadir = os.path.join(os.getenv('SST_OOD'),
+                                       'MODIS_L2', 'Models',
+                                       'R2019_2010_128x128_std')
+            else:  # s3
+                datadir = os.path.join('s3://modis-l2', 'Models',
+                                       'R2019_2010_128x128_std')
+        if filepath is None:
+            filepath = 'PreProc/MODIS_R2019_2010_95clear_128x128_preproc_std.h5'
+    
+    # Load JSON
+    json_file = os.path.join(datadir , 'model.json') 
+    with ulmo_io.open(json_file, 'rt') as fh:
+        model_dict = json.load(fh)
+    
+    # Instantiate
+    pae = ood.ProbabilisticAutoencoder.from_dict(
+        model_dict,
+        datadir=datadir,
+        filepath=filepath,
+        logdir=datadir)
+
+    # Giddy up                                            
+    pae.load_autoencoder()
+    pae.load_flow()
+
+    # Load scaler
+    scaler_path = os.path.join(pae.logdir, pae.stem + '_scaler.pkl')
+    with ulmo_io.open(scaler_path, 'rb') as f:
+        pae.scaler = pickle.load(f)
+    print("scaler loaded from: {}".format(scaler_path))
+
+    # Return
+    return pae
