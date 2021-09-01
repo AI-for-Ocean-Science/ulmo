18dff1ba60263db04d364e5345e3e5e047903774
karlschwarz <eguo1@ucsc.edu>
2021-09-01 17:41:09 +0000

    extend main function to support finding similar images of a given image file.

--- ulmo/scripts/ssl_similar.py
+++ ulmo/scripts/ssl_similar.py
@@ -1,4 +1,8 @@
 """ Script to grab LLC model data """
+### used to set the interpreter searching path
+import sys
+target_path = '/home/jovyan/ulmo/'
+sys.path.append(target_path)
 
 # Have to do this here, so odd..
 import pickle
@@ -11,9 +15,11 @@ def parser(options=None):
     # Parse
     parser = argparse.ArgumentParser(description='Grab Similar SSL Images')
     parser.add_argument("pp_file", type=str, help="Pre-process file; can be s3")
-    parser.add_argument("pp_idx", type=int, help="Pre-process identifier")
+    parser.add_argument("--pp_idx", default=None, type=int, help="Pre-process identifier")
     parser.add_argument("model", type=str, help="Name of SSL model [LLC]")
     parser.add_argument("dataset", type=str, help="Name of dataset [LLC]")
+    parser.add_argument("--img_path", default=None, type=str, help="Path of the target image")
+    parser.add_argument("--num_imgs", default=5, type=int, help="Number of images to be searched")
 
     if options is None:
         pargs = parser.parse_args()
@@ -25,6 +31,8 @@ def load_umap(pargs):
     print("Loading UMAP")
     if pargs.model == 'LLC':
         umap_file = 's3://llc/SSL/LLC_MODIS_2012_model/ssl_LLC_v1_umap.pkl'
+    elif pargs.model == 'LLC_local':
+        umap_file = './ssl_LLC_v1_umap.pkl'
     else:
         raise IOError("bad model")
     umap_base = os.path.basename(umap_file)
@@ -59,7 +67,11 @@ def build_gallery(pargs, img, data_tbl, srt,
     _, cm = plotting.load_palette()
     fig = plt.figure(figsize=(12, 6))
     plt.clf()
-    gs = gridspec.GridSpec(2,3)
+    n_col = 4
+    n_rows = n_new // n_col + 1 if n_new % n_col else n_new // n_col
+    #gs = gridspec.GridSpec(2,3)
+    gs = gridspec.GridSpec(n_rows,n_col)
+    
 
     # Input image
     vmin, vmax = img.min(), img.max()
@@ -98,32 +110,50 @@ def main(pargs):
 
     import torch
     import h5py
+    
+    from PIL import Image
 
     from ulmo import io as ulmo_io
     from ulmo.ssl.train_util import Params, option_preprocess
     from ulmo.ssl import latents_extraction
 
     # Prep
-    if pargs.model == 'LLC':
+    if pargs.model == 'LLC' or pargs.model == 'LLC_local':
         model_file = 's3://llc/SSL/LLC_MODIS_2012_model/SimCLR_LLC_MODIS_2012_resnet50_lr_0.05_decay_0.0001_bsz_64_temp_0.07_trial_0_cosine_warm/last.pth'
         opt_file = os.path.join(resource_filename('ulmo', 'runs'),
                                 'SSL', 'LLC', 'experiments', 
                                 'llc_modis_2012', 'opts.json')
         table_file = 's3://llc/Tables/LLC_MODIS2012_SSL_v1.parquet'
+        
     else:
         raise IOError("Bad model!!")
-
+    
+    if not (pargs.pp_idx or pargs.img_path):
+        raise IOError("One argument of 'pp_idx' and 'img_path' must be valued.")
+    
+    if pargs.pp_idx and pargs.img_path:
+        raise IOError("Only one argument of 'pp_idx' and 'img_path' can be used.")
+        
     # Load the image
     print("Loading image..")
-    with ulmo_io.open(pargs.pp_file, 'rb') as f:
-        pp_hf = h5py.File(f, 'r')
-    #img = pp_hf['valid'][pargs.pp_idx:pargs.pp_idx+1, ...]
-    img = pp_hf['valid'][pargs.pp_idx:pargs.pp_idx+64, ...]
-    pp_hf.close()
-    img = np.repeat(img, 3, axis=1)
-    print(f"Image with index={pargs.pp_idx} loaded from {pargs.pp_file}")
-
-
+    if pargs.pp_idx:
+        with ulmo_io.open(pargs.pp_file, 'rb') as f:
+            pp_hf = h5py.File(f, 'r')
+        #img = pp_hf['valid'][pargs.pp_idx:pargs.pp_idx+1, ...]
+        img = pp_hf['valid'][pargs.pp_idx:pargs.pp_idx+64, ...]
+        pp_hf.close()
+        img = np.repeat(img, 3, axis=1)
+        print(f"Image with index={pargs.pp_idx} loaded from {pargs.pp_file}") 
+    else:
+        img = Image.open(pargs.img_path)
+        img = np.array(img.resize((64, 64)))
+        if img.shape[2] != 3:
+            img = np.mean(img, axis=-1, keepdims=True)
+            img = np.repeat(img, 3, axis=-1)
+        img = np.transpose(img, (2, 0, 1))
+        img = np.expand_dims(img, axis=0)
+        print(f"Target Image is loaded and pre-processd!")
+        
     model_base = os.path.basename(model_file)
     if not os.path.isfile(model_base):
         ulmo_io.download_file_from_s3(model_base, model_file)
@@ -151,7 +181,7 @@ def main(pargs):
     srt_dist = np.argsort(dist)
 
     # Gallery time
-    build_gallery(pargs, img[0,0,...], data_tbl, srt_dist)
+    build_gallery(pargs, img[0,0,...], data_tbl, srt_dist, n_new=pargs.num_imgs)
     
     # Leave no trace
     os.remove(model_base)
