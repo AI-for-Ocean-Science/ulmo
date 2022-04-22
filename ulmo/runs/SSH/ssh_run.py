a68e0764b283d2c8f4ed656f990b704a62e334af
BenPritikin <bpritiki@ucsc.edu>
2022-04-22 12:32:42 -0700

    modified ssh_run.py for no inpainting

--- ulmo/runs/SSH/ssh_run.py
+++ ulmo/runs/SSH/ssh_run.py
@@ -131,7 +131,7 @@ def ssh_extraction(pargs, n_cores=20,
                                 maxshape=(None, fields.shape[1], fields.shape[2]))
             f_h5.create_dataset('inpainted_masks', data=inpainted_masks,
                                 compression="gzip", chunks=True,
-                                maxshape=(None, inpainted_masks.shape[1], inpainted_masks.shape[2]))
+                                maxshape=(None, inpainted_masks.shape[0], inpainted_masks.shape[0]))
         else:
             # Resize
             for key in ['fields', 'inpainted_masks']:
