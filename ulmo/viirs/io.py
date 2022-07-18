cbe41cbac950ed3d09fe7b9edb7c03bd474fec2f
profxj <xavier@ucolick.org>
2022-07-18 10:13:39 -0700

    corrected I/O

--- ulmo/viirs/io.py
+++ ulmo/viirs/io.py
@@ -13,8 +13,9 @@ def load_nc(filename, verbose=True):
     verbose : bool, optional
     Returns
     -------
-    ssh, latitude, longitude : np.ndarray, np.ndarray, np.ndarray np.ndarray
-        Height map
+    sst, qual, latitude, longitude : np.ndarray, np.ndarray, np.ndarray np.ndarray
+        Temperture map
+        Quality
         Latitutides
         Longitudes
         or None's if the data is corrupt!
@@ -23,19 +24,21 @@ def load_nc(filename, verbose=True):
         #inp = ulmo_io.load_to_bytes(filename)
         with ulmo_io.open(filename, 'rb') as f:
             ds = xarray.open_dataset(filename_or_obj=f,
-                engine='netcdf4')
-                #mask_and_scale=True)
+                engine='h5netcdf',
+                mask_and_scale=True)
     else:
         inp = filename
         ds = xarray.open_dataset(filename_or_obj=inp,
-            engine='netcdf4')
-            #mask_and_scale=True)
+            engine='h5netcdf',
+            mask_and_scale=True)
 
     try:
         # Fails if data is corrupt
-        ssh = ds.SLA.data[0,...]
-        latitude = ds.Latitude.data[:]
-        longitude = ds.Longitude.data[:]
+        sst = ds.sea_surface_temperature.data[0,...] - 273.15 # Celsius!
+        qual = ds.quality_level.data[0,...].astype(int)
+        #qual = ds.l2p_flags.data[0,...]
+        latitude = ds.lat.data[:]
+        longitude = ds.lon.data[:]
     except:
         if verbose:
             print("Data is corrupt!")
@@ -44,4 +47,4 @@ def load_nc(filename, verbose=True):
     ds.close()
 
     # Return
-    return ssh, latitude, longitude
+    return sst, qual, latitude, longitude
