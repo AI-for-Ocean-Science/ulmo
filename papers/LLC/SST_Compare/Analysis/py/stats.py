e9095435a62228b59baa8cdf4a09a553f08311fc
profxj <xaiver@ucolick.org>
2022-09-11 21:46:59 -0700

    tables for Peter

---
+++ papers/LLC/SST_Compare/Analysis/py/stats.py
@@ -0,0 +1,91 @@
+""" Methods for stats on the outputs """
+import numpy as np
+import healpy as hp
+
+import pandas
+
+from ulmo import io as ulmo_io
+
+from IPython import embed
+
+def healpix_stats(outfile:str, local=False, debug:bool=False,
+                  nside:int=64, cut:str=None, llc:bool=False):
+
+    # Load table
+    if not llc:
+        if local:
+            table_file = '/home/xavier/Projects/Oceanography/SST/VIIRS/Tables/VIIRS_all_98clear_std.parquet'
+        else:
+            table_file = 's3://viirs/Tables/VIIRS_all_98clear_std.parquet'
+    else:
+        if local:
+            table_file = '/home/xavier/Projects/Oceanography/SST/LLC/Tables/llc_viirs_match.parquet'
+        
+    eval_tbl = ulmo_io.load_main_table(table_file)
+
+    # Heads
+    if cut is not None:
+        if cut == 'head':
+            cutt = (eval_tbl.datetime.dt.year > 2011) & (
+                eval_tbl.datetime.dt.year < 2015) 
+        elif cut == 'tail':
+            cutt = (eval_tbl.datetime.dt.year > 2017) & (
+                eval_tbl.datetime.dt.year < 2021) 
+        else:
+            raise IOError("Bad cut")
+        eval_tbl = eval_tbl[cutt].copy()
+
+    # Healpix
+    lats = eval_tbl.lat.values
+    lons = eval_tbl.lon.values
+
+    # Healpix coords
+    theta = (90 - lats) * np.pi / 180.
+    phi = lons * np.pi / 180.
+    idx_all = hp.pixelfunc.ang2pix(nside, theta, phi)
+
+    # Stats
+    npix_hp = hp.nside2npix(nside)
+    sigma = np.zeros(npix_hp)
+    mean = np.zeros(npix_hp)
+    median = np.zeros(npix_hp)
+    counts = np.zeros(npix_hp, dtype=int)
+
+    uni_idx = np.unique(idx_all)
+    count = 0
+    for idx in uni_idx:
+        #print(f'{count} of {len(uni_idx)}')
+        gd = idx_all == idx
+        if np.sum(gd) > 2:
+            sigma[idx] = np.std(eval_tbl[gd].LL.values)
+            mean[idx] = np.mean(eval_tbl[gd].LL.values)
+            median[idx] = np.median(eval_tbl[gd].LL.values)
+            counts[idx] = np.sum(gd)
+        count += 1
+        if debug and count > 100:
+            break
+
+    # Output
+    df = pandas.DataFrame()
+
+    df['sigma'] = sigma
+    df['mean'] = mean
+    df['median'] = median
+    df['N'] = counts
+    df['idx'] = np.arange(npix_hp)
+
+    df.to_csv(outfile, index=False)
+    print(f'Wrote: {outfile}')
+
+# Command line execution
+if __name__ == '__main__':
+
+    # All
+    #healpix_stats('all_viirs.csv', local=True)#, debug=True)
+
+    # Head/tail
+    #healpix_stats('head_viirs.csv', local=True, cut='head')
+    #healpix_stats('tail_viirs.csv', local=True, cut='tail')
+
+    # LLC
+    healpix_stats('all_llc.csv', local=True, llc=True)
