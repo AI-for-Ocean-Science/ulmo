1949d69ac66da9ab121eeb91cf2366891980c38f
aagabin <aagabin@ucsc.edu>
2022-11-10 22:46:15 +0000

    added test model for LLC

--- ulmo/mae/models_mae.py
+++ ulmo/mae/models_mae.py
@@ -235,10 +235,10 @@ def mae_vit_large_patch16_dec512d8b(**kwargs):
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
-def mae_vit_testt(**kwargs):
+def mae_vit_LLC_patch4(**kwargs):
     model = MaskedAutoencoderViT(
-        patch_size=4, embed_dim=256, depth=12, num_heads=8,
-        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=8,
+        patch_size=4, embed_dim=768, depth=12, num_heads=12,
+        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
