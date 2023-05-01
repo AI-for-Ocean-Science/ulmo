6d1f161e86a15636f653444a195654be1345de30
aagabin <aagabin@ucsc.edu>
2023-05-01 05:47:52 +0000

    still might not be working right

---
+++ papers/MAE/Figures/py/figs_galleries.py
@@ -0,0 +1,155 @@
+""" Create SST cutout galleries """
+from dataclasses import replace
+from datetime import datetime
+import os, sys
+import numpy as np
+import scipy
+from scipy import stats
+from urllib.parse import urlparse
+import datetime
+
+import argparse
+
+import healpy as hp
+
+import matplotlib as mpl
+import matplotlib.gridspec as gridspec
+from matplotlib import pyplot as plt
+from matplotlib.patches import Rectangle, Ellipse
+
+
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+
+mpl.rcParams['font.family'] = 'stixgeneral'
+
+import pandas as pd
+import seaborn as sns
+
+import h5py
+
+from ulmo import plotting
+from ulmo.utils import utils as utils
+
+from ulmo import io as ulmo_io
+from ulmo.ssl import single_image as ssl_simage
+from ulmo.ssl import defs as ssl_defs
+from ulmo.mae import patch_analysis
+from ulmo.utils import image_utils
+
+
+def patches(orig_img, mask_img, p_sz):
+    # Find the patches
+    p_sz = 4
+    unmasked = 1 - mask_img
+    patches = patch_analysis.find_patches(mask_img, p_sz)
+    upatches = patch_analysis.find_patches(unmasked, p_sz)
+    
+    # Reconstructed
+    sub_recon = np.ones_like(orig_img) * np.nan
+    
+    # Plot/fill the patches
+    for kk, patch in enumerate(upatches):
+        i, j = np.unravel_index(patch, unmasked.shape)
+        # Fill
+        sub_recon[i:i+p_sz, j:j+p_sz] = orig_img[i:i+p_sz, j:j+p_sz]
+    return sub_recon
+    
+
+def plot_gallery(imgs_file, outfile='mask_ratio_gallery.png', vmnx = [None, None]):
+    """
+    Gallery to show how different models perform at different masking ratios
+    default file to run this is 'idx330469_recons.npz' uploaded in the Enki google doc
+    imgs_file: npz file containing 
+               1) 'img' containing the original image
+               2) 'recons' with p=10,30,50 reconstructions 
+                   for t=10,35,50,75 in that order (i.e. imgs[1][2] is p=30, t=35 recons)
+               3) 'masks' with the mask for p=10,30,50 in that order
+    outfile:   Name of output file
+    vmnx:      The lower and upper limits for the colorbar
+    """
+    imgs = np.load(imgs_file)
+    orig_img = imgs['img']
+    p = imgs['recons']
+    masks = imgs['masks']
+    print(np.max(orig_img))
+    print(np.min(orig_img))
+    
+    fig = plt.figure(figsize=(12, 7))
+    #fig.tight_layout()
+    plt.clf()
+    gs = gridspec.GridSpec(3,6)
+    ax0 = plt.subplot(gs[1:2, 0])
+    
+    _, cm = plotting.load_palette()
+    cbar_kws={'label': 'SSTa (K)',
+              'fraction': 0.0450,
+              'location': 'top'}
+    
+    # Plot original image
+    _ = sns.heatmap(np.flipud(orig_img), xticklabels=[],
+                    vmin=vmnx[0], vmax=vmnx[1],
+                    yticklabels=[], cmap=cm, cbar=True, 
+                    square=True, 
+                    cbar_kws=cbar_kws,
+                    ax=ax0)
+    
+    # Plot Masks
+    ax1 = plt.subplot(gs[0, 1])
+    sub_recon = patches(orig_img, masks[0], 4)
+    _ = sns.heatmap(np.flipud(sub_recon), xticklabels=[],
+                vmin=vmnx[0], vmax=vmnx[1],
+                yticklabels=[], cmap=cm, cbar=False, 
+                square=True, ax=ax1)
+    ax2 = plt.subplot(gs[1, 1])
+    sub_recon = patches(orig_img, masks[1], 4)
+    _ = sns.heatmap(np.flipud(sub_recon), xticklabels=[],
+                vmin=vmnx[0], vmax=vmnx[1],
+                yticklabels=[], cmap=cm, cbar=False, 
+                square=True, ax=ax2)
+    ax3 = plt.subplot(gs[2, 1])
+    sub_recon = patches(orig_img, masks[2], 4)
+    _ = sns.heatmap(np.flipud(sub_recon), xticklabels=[],
+                vmin=vmnx[0], vmax=vmnx[1],
+                yticklabels=[], cmap=cm, cbar=False, 
+                square=True, ax=ax3)
+    
+    # Plot reconstructions
+    model = [10,35,50,75]
+    for i in range(3):
+        for j in range(4):
+            ax = plt.subplot(gs[i, j+2])
+            ax.patch.set_edgecolor('black')  
+            ax.patch.set_linewidth(1.)
+            ax.text(2,6, 't{}'.format(model[j]), c='w', weight='bold', fontsize='12')
+            #ax.text(2,6, 't{}'.format(model[j]), c='k', weight='bold', fontsize='12')
+            _ = sns.heatmap(np.flipud(p[i][j]), xticklabels=[],
+                    vmin=vmnx[0], vmax=vmnx[1],
+                    yticklabels=[], cmap=cm, cbar=False, 
+                    square=True, ax=ax)
+    fig.subplots_adjust(wspace=0.1, hspace=0)
+    
+     # Borders
+    for ax, title in zip( [ax0, ax1, ax2 ,ax3],
+        ['original', 'p = 10', 'p = 30', 'p = 50']):
+        ax.patch.set_edgecolor('black')  
+        ax.patch.set_linewidth(1.)  
+        
+        show_title=True
+        if show_title:
+            ax.set_title(title, fontsize=14, y=-0.18)
+    
+    # Adjust whitespace and plot position
+    fig.tight_layout()
+    fig.subplots_adjust(bottom=0.05)
+    
+    # TODO: move colorbar to bottom or side of plot
+    
+    # Save
+    plt.savefig(outfile, dpi=300)
+    plt.close()
+    print(f'Wrote: {outfile}')
+    return
+    
+#plot_gallery("idx330469_recons.npz", vmnx = [-9.1,6.3])
+#plot_gallery("idx473815_recons.npz", vmnx = [-5.6,5])
+plot_gallery("idx92810_recons.npz", vmnx = [-9, 6.3])
---
+++ papers/MAE/Figures/py/figs_rmse.py
@@ -0,0 +1,115 @@
+import sys
+import os
+import requests
+import time
+
+import torch
+import numpy as np
+import pandas as pd
+import h5py
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from PIL import Image
+import matplotlib.ticker as ticker
+import matplotlib.gridspec as gridspec
+
+from ulmo.plotting import plotting
+from ulmo import io as ulmo_io
+
+from IPython import embed
+import matplotlib as mpl
+mpl.rcParams['font.family'] = 'stixgeneral'
+
+def fig_batch_rmse(model, rmse_filepath='valid_avg_rms.csv'):
+    """
+    Creates a figure of average RMSE by LL batches for a single image.
+    model: MAE model (10, 35, 50, 75)
+    rmse_filepath: file with rmse's
+    """
+    # load rmse
+    rmse = pd.read_csv(rmse_filepath)
+    
+    # Plot
+    fig, ax = plt.subplots()
+    
+    masks = [10, 20, 30, 40, 50]
+    colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
+    plt_labels = []
+    for p, c in zip(masks, colors):
+        x = rmse['median_LL']
+        y = rmse['rms_t{t}_p{p}'.format(t=model, p=p)]
+        plt_labels.append('p={p}%'.format(p=p))
+        plt.scatter(x, y, color=c)
+
+    ax.set_axisbelow(True)
+    ax.grid(color='gray', linestyle='dashed', linewidth = 0.5)
+    plt.legend(labels=plt_labels, title='Masking Ratio',
+               title_fontsize='small', fontsize='small', fancybox=True)
+    plt.title('Average RMSE vs LL: t={}'.format(model))
+    plt.xlabel("Median LL Per Batch")
+    plt.ylabel("RMSE")
+
+    # save
+    outfile = 'rmse_t{}.png'.format(model)
+    plt.savefig(outfile, dpi=300)
+    plt.close()
+
+    return
+
+def figs_rmse_all_models(outfile='rmse_models.png',rmse_filepath='valid_avg_rms.csv'):
+    # load rmse
+    rmse = pd.read_csv(rmse_filepath)
+    
+    fig = plt.figure(figsize=(10, 10))
+    
+    plt.clf()
+    gs = gridspec.GridSpec(2,2)
+    
+    models = [10,35,50,75]
+    masks = [10,20,30,40,50]
+    colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
+    plt_labels = []
+    for i in range(4):
+        
+        # determine position of plot
+        pos = [0, i]
+        if i > 1:
+            pos = [1, i-2]
+        ax = plt.subplot(gs[pos[0], pos[1]])
+        
+        # plot
+        for p, c in zip(masks, colors):
+            x = rmse['median_LL']
+            y = rmse['rms_t{t}_p{p}'.format(t=models[i], p=p)]
+            plt_labels.append('p={p}%'.format(p=p))
+            plt.scatter(x, y, color=c)
+
+        if models[i] != 50:
+            ax.set_ylim([0, 0.15])
+        ax.set_axisbelow(True)
+        ax.grid(color='gray', linestyle='dashed', linewidth = 0.5)
+        plt.legend(labels=plt_labels, title='Masking Ratio',
+                   title_fontsize='small', fontsize='small', fancybox=True)
+        plt.title('t={}'.format(models[i]))
+        plt.xlabel("Median LL Per Batch")
+        plt.ylabel("Average RMSE")
+                         
+    fig.tight_layout()
+    fig.subplots_adjust(top=0.92)
+    fig.subplots_adjust(wspace=0.2)
+    fig.suptitle('RMSE vs LL', fontsize=16)
+    
+    outfile = 'rmse_vs_LL.png'
+    plt.savefig(outfile, dpi=300)
+    plt.close()
+    return
+    
+figs_rmse_all_models()
+
+'''
+models = [10,35,50,75]
+
+for t in models:
+    fig_batch_rmse(t)
+'''
