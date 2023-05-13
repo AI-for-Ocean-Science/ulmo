95eff7f613607c0b31ed161e2c6d7d1eb299f8b9
J. Xavier Prochaska <xavier@ucolick.org>
2023-05-13 15:09:59 -0700

    name

--- papers/MAE/Figures/py/mae_figs_thesis.py
+++ papers/MAE/Figures/py/mae_figs_thesis.py
@@ -287,7 +287,9 @@ def plot_recon(orig_img, recon_img, recon_full, mask_img, idx,
     print('Wrote {:s}'.format(outfile))
 
 def figs_training(idx=85674, filepath='data/MAE_LLC_valid_nonoise_preproc.h5', 
-                  model_filepath='data/ENKI_t75.pth',
+                  model_filepath=os.path.join(os.getenv('OS_OGCM'),
+                                              'LLC', 'Enki', 'Models',
+                                              'ENKI_t75.pth'),
                   table = 'data/MAE_LLC_valid_nonoise.parquet'):
     """
     Create fig
