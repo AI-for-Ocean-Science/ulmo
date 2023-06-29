045e7fa602d55905c001646d1ab89c211f9dd5eb
profxj <xavier@ucolick.org>
2023-06-29 07:59:15 -0700

    still sufferin

--- papers/Enki/Analysis/py/anly_patches.py
+++ papers/Enki/Analysis/py/anly_patches.py
@@ -1,14 +1,14 @@
 """ Routines to analyze patches """
 import numpy as np
 
-
+import h5py
 
 from ulmo.mae import patch_analysis
 from ulmo.mae import enki_utils
 
+from ulmo import io as ulmo_io
+
 from IPython import embed
-
-
 
 if __name__ == "__main__":
 
@@ -49,7 +49,6 @@ if __name__ == "__main__":
     print(f"Working on: {recon_file} using orig={orig_file}")
     patch_analysis.anlayze_full(
         recon_file, orig_file=orig_file, bias=bias, nsub=100000, n_cores=12) 
-    '''
 
     # LLC2 noise but with noiseless patches
     t=10
@@ -66,3 +65,48 @@ if __name__ == "__main__":
         recon_file, orig_file=orig_file, bias=bias, 
         nsub=100000, n_cores=12,
         outfile='enki_noise_patches_noiseless_t10_p10.npz')
+    '''
+
+    # Generate aligned orig file
+    t=10
+    p=10
+    tbl_file, _, recon_file, mask_file = enki_utils.set_files(
+        dataset='LLC2_noise', t=t, p=p)
+    tbl_file2, orig_file, _, _ = enki_utils.set_files(
+        dataset='LLC2_nonoise', t=t, p=p)
+
+    # Load tables
+    noise_tbl = ulmo_io.load_table(tbl_file)
+    nonoise_tbl = ulmo_io.load_table(tbl_file2)
+
+    # Load up orig_file
+    f_orig = h5py.File(orig_file, 'r')
+    orig_imgs = f_orig['valid'][:]
+
+    # Ugly for loop..
+    new_orig = np.zeros_like(orig_imgs)
+
+    embed(header='89 of anly_patches.py')
+    for ss in len(noise_tbl):
+        if ss % 1000 == 0:
+            print(f'ss: {ss}')
+        if noise_tbl.iloc[ss].pp_idx < 0:
+            continue
+        #
+        noise_idx = noise_tbl.iloc[ss].pp_idx
+        nonoise_idx = nonoise_tbl.iloc[ss].pp_idx
+        #
+        new_orig[noise_idx] = orig_imgs[nonoise_idx]
+
+    # Write 
+    # ###################
+    outfile = 'Enki_LLC_valid_noise_nonoise_preproc.h5'
+    print("Writing: {}".format(outfile))
+    with h5py.File(outfile, 'w') as f:
+        # Validation
+        f.create_dataset('valid', data=new_orig.astype(np.float32))
+    print("Wrote: {}".format(outfile))
+    
+    # Upload
+    ulmo_io.upload_file_to_s3(
+        outfile, 's3://llc/mae/PreProc/'+outfile)
