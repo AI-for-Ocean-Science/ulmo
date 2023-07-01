175a4fb22ecb83de0ae686e8c6761ce8320a327b
profxj <xavier@ucolick.org>
2023-07-01 05:32:20 -0700

    renaming

--- ulmo/nenya/io.py
+++ ulmo/nenya/io.py
@@ -6,20 +6,34 @@ from ulmo import io as ulmo_io
 from ulmo.nenya.train_util import option_preprocess
 
 def load_opt(model:str):
+    """ Load the SSL model options
+
+    Args:
+        model (str): name of the model
+
+    Raises:
+        IOError: _description_
+
+    Returns:
+        tuple: SSL options, model file (str)
+    """
     # Prep
     model_file = None
     if model == 'LLC' or model == 'LLC_local':
         model_file = 's3://llc/SSL/LLC_MODIS_2012_model/SimCLR_LLC_MODIS_2012_resnet50_lr_0.05_decay_0.0001_bsz_64_temp_0.07_trial_0_cosine_warm/last.pth'
         opt_path = os.path.join(resource_filename('ulmo', 'runs'),
-                                'SSL', 'LLC', 'experiments', 
+                                'Nenya', 'LLC', 'experiments', 
                                 'llc_modis_2012', 'opts.json')
     elif model == 'CF': 
         opt_path= os.path.join(resource_filename('ulmo', 'runs'),
-            'SSL', 'MODIS', 'v2', 'experiments',
+            'Nenya', 'MODIS', 'v2', 'experiments',
             'modis_model_v2', 'opts_cloud_free.json')
-    elif model == 'v4': 
+    elif model == 'v4':  
         opt_path= os.path.join(resource_filename('ulmo', 'runs'),
-            'SSL', 'MODIS', 'v4', 'opts_ssl_modis_v4.json')
+            'Nenya', 'MODIS', 'v4', 'opts_ssl_modis_v4.json')
+    elif model == 'v5': 
+        opt_path= os.path.join(resource_filename('ulmo', 'runs'),
+            'Nenya', 'MODIS', 'v5', 'opts_nenya_modis_v5.json')
     else:
         raise IOError("Bad model!!")
 
